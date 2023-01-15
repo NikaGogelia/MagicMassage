f1f596c0e1267a0df8607e3e810deb7b50d7921f
Nika Gogelia <gogelianika1@gmail.com>
2023-01-15 18:23:39 +0400

    add responsiveness on desktop size

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 "use strict";
 
+// Navbar Features
+const navLinks = document.querySelectorAll(".nav-links li");
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    navLinks.forEach((n) => n.classList.remove("nav-active"));
+    navLink.classList.add("nav-active");
+  });
+});
+
 // Accordion Features
 const accordions = document.querySelectorAll(".accordion");
 
 accordions.forEach((accordion) => {
-  accordion.addEventListener("click", (e) => {
-    // accordions.forEach((accordion) =>
-    //   accordion.classList.remove("activate-accordion")
-    // );
-    accordion.classList.toggle("activate-accordion");
+  accordion.addEventListener("click", () => {
+    if (!accordion.classList.contains("activate-accordion")) {
+      accordions.forEach((a) => a.classList.remove("activate-accordion"));
+      accordion.classList.add("activate-accordion");
+    } else {
+      accordion.classList.remove("activate-accordion");
+    }
   });
 });
